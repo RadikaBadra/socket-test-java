dcdc694d061b6948db596c75551fd19f20cc8619
Radika Badragtiha <puturadika69@gmail.com>
2023-04-03 13:33:07 +0800

    initialize project

---
+++ src/tessocket/Server.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tessocket;
+import java.net.*;
+
+import java.io.*;
+
+public class Server {
+
+public static void main(String[] args) {
+
+    try {
+        ServerSocket ss = new ServerSocket(3000);
+        System.out.println("Menunggu Klien...");
+        Socket s = ss.accept();
+        System.out.println("Klien telah masuk");
+        System.out.println("Server pada port = "+
+        s.getLocalAddress().getHostAddress() + " : " + s.getLocalPort());
+        System.out.println("Client pada port = "+
+        s.getInetAddress().getHostAddress() + " : " + s.getPort());
+        
+//        BufferedReader op = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//        System.out.println("Menunggu pesan...");
+//        String output = "";
+//        while ((output=op.readLine())!=null) { System.out.println(output);
+//        }
+   
+        PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader ip = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+
+        while(!(input=ip.readLine()).equals("")) { pw.println(input);
+        }     
+        System.out.println("Selesai");
+//        op.close();
+        ip.close();
+        pw.close();
+        s.close(); ss.close();
+    }
+
+    catch (Exception e) { 
+        e.printStackTrace(); 
+    }
+}
+
+}
+
